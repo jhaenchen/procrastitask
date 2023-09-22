1d635389f8fbdd39f24c4b6e605764674dfa3285
Jackson Haenchen <jacksonhaenchen@gmail.com>
2023-09-21 22:02:07 -0500

    Editing works

--- procrastitask.py
+++ procrastitask.py
@@ -1,10 +1,38 @@
 import json
 import os
 import random
+import tempfile
+from subprocess import call
+from time import sleep
+
+EDITOR = os.environ.get("EDITOR", "vim")  # that easy!
+
+
+def rlinput(prefill: str, prompt="Edit:"):
+    initial_message = bytes(
+        str(prompt) + str(prefill), encoding="utf-8"
+    )  # if you want to set up the file somehow
+
+    with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
+        tf.write(initial_message)
+        tf.flush()
+        call([EDITOR, "+set backupcopy=yes", tf.name])
+
+        # do the parsing with `tf` using regular File operations.
+        # for instance:
+        tf.seek(0)
+        edited_message = tf.read()
+        splitted = str(edited_message).split(prompt)
+        if len(splitted) == 2:
+            return splitted[1][:-3]
+        return ""
+
 
 class Task:
     def pretty_print(self):
-        print(f"\n{self.title} ({self.duration}min, stress: {self.stress, diff: {self.difficulty}})\n")
+        print(
+            f"\n{self.title} ({self.duration}min, stress: {self.stress}, diff: {self.difficulty})\n"
+        )
         print(f"{self.description}\n")
 
     def headline(self):
@@ -13,7 +41,9 @@ class Task:
     def complete(self):
         self.is_complete = True
 
-    def __init__(self, title, description, duration, stress, difficulty, is_complete = False):
+    def __init__(
+        self, title, description, duration, stress, difficulty, is_complete=False
+    ):
         self.title = title
         self.description = description
         self.duration = duration
@@ -29,7 +59,7 @@ class Task:
             incoming_dict["duration"],
             incoming_dict["stress"],
             incoming_dict["difficulty"],
-            incoming_dict["is_complete"]
+            incoming_dict["is_complete"],
         )
 
     def to_dict(self):
@@ -39,8 +69,9 @@ class Task:
             "duration": self.duration,
             "stress": self.stress,
             "difficulty": self.difficulty,
-            "is_complete": self.is_complete
+            "is_complete": self.is_complete,
         }
+
 
 class App:
     def __init__(self):
@@ -49,29 +80,30 @@ class App:
         self.reset_screen()
 
     def load(self):
-        with open("tasks.json", "r") as db:
+        with open("/Users/haenchen/tasks.json", "r") as db:
             json_tasks = json.loads(db.read())
             self.all_tasks = [Task.from_dict(j_task) for j_task in json_tasks]
 
     def save(self):
-        with open("tasks.json", "w") as db:
+        with open("/Users/haenchen/tasks.json", "w") as db:
             task_json_dicts = [task.to_dict() for task in self.all_tasks]
             json_str = json.dumps(task_json_dicts)
             db.write(json_str)
 
     def delete_task(self, task_title):
-        #print(f"Deleting title {task_title} from collection {self.all_tasks}")
-        self.all_tasks = [task for task in self.all_tasks if task.title != task_title]    
+        # print(f"Deleting title {task_title} from collection {self.all_tasks}")
+        self.all_tasks = [task for task in self.all_tasks if task.title != task_title]
 
-    def get_numerical_prompt(self, prompt_text, also_accept=None):
+    def get_numerical_prompt(self, prompt_text, also_accept=None, input_func=None):
         while True:
             try:
-                result = input(prompt_text)
+                result = input_func(prompt_text) if input_func else input(prompt_text)
                 return int(result)
             except ValueError:
-                if result in also_accept:
+                if result in (also_accept or []):
                     return result
-                print("\nBad input. Try again.\n")
+                print(f"\nBad input: {result}. Try again.\n")
+                sleep(5)
 
     def create_new_task(self):
         task_title = input("Enter your task: ")
@@ -79,15 +111,21 @@ class App:
         duration = self.get_numerical_prompt("Estimated duration (minutes): ")
         stress_level = self.get_numerical_prompt("Stress level: ")
         difficulty = self.get_numerical_prompt("Difficulty: ")
-        created_task = Task(task_title, task_description, duration, stress_level, difficulty)
+        created_task = Task(
+            task_title, task_description, duration, stress_level, difficulty
+        )
         return created_task
 
-    def list_all_tasks(self, task_list_override = None, extend_cache = False):
+    def list_all_tasks(self, task_list_override=None, extend_cache=False):
         tasks = task_list_override or self.all_tasks
         if not extend_cache:
             self.cached_listed_tasks = {}
         incomplete_tasks = [task for task in tasks if task.is_complete == False]
-        start_index = 0 if not extend_cache else (max(-1, *[key for key in self.cached_listed_tasks]) + 1)
+        start_index = (
+            0
+            if not extend_cache
+            else (max(-1, *[key for key in self.cached_listed_tasks]) + 1)
+        )
         if len(incomplete_tasks) == 0:
             print("You have no available tasks.")
         incomplete_tasks.sort(key=lambda t: t.stress, reverse=True)
@@ -107,7 +145,9 @@ class App:
     def _get_strictly_matching_tasks(self, available_time, available_energy):
         candidates = []
         for task in self.all_tasks:
-            if (task.duration <= available_time) and (task.difficulty <= available_energy):
+            if (task.duration <= available_time) and (
+                task.difficulty <= available_energy
+            ):
                 candidates.append(task)
         candidates.sort(key=lambda t: t.stress)
         return candidates
@@ -115,17 +155,26 @@ class App:
     def _get_stretch_tasks(self, available_time, available_energy):
         candidates = []
         for task in self.all_tasks:
-            if (task.duration < available_time) and ((task.difficulty <= (int(available_energy * 1.5)) and (task.difficulty > available_energy))):
+            if (task.duration < available_time) and (
+                (
+                    task.difficulty <= (int(available_energy * 1.5))
+                    and (task.difficulty > available_energy)
+                )
+            ):
                 candidates.append(task)
         candidates.sort(key=lambda t: t.stress)
         return candidates
 
     def wizard(self):
         print("\nWelcome to the completion wizard.")
-        available_time = self.get_numerical_prompt("\nHow much time do you have (minutes)? ")
+        available_time = self.get_numerical_prompt(
+            "\nHow much time do you have (minutes)? "
+        )
         available_energy = self.get_numerical_prompt("\nHow much energy do you have? ")
         self.cached_listed_tasks = {}
-        strict_candidates = self._get_strictly_matching_tasks(available_time, available_energy)
+        strict_candidates = self._get_strictly_matching_tasks(
+            available_time, available_energy
+        )
         stretch_candidates = self._get_stretch_tasks(available_time, available_energy)
         self.reset_screen()
         if len(strict_candidates) > 0:
@@ -138,17 +187,21 @@ class App:
             if stretch_candidates:
                 print("\nYou have no perfect fits, but try these stretch tasks:")
                 self.list_all_tasks([stretch_candidates[:3]])
-        
+
     def find_task(self, task_title):
         matches = [task for task in self.all_tasks if task.title == task_title]
         return matches[0] if matches else None
-    
+
     def refresh_stress_levels(self):
         self.reset_screen()
         seen_tasks = set()
         while True:
             self.reset_screen()
-            remaining_tasks = [t for t in self.all_tasks if t.is_complete == False and t not in seen_tasks]
+            remaining_tasks = [
+                t
+                for t in self.all_tasks
+                if t.is_complete == False and t not in seen_tasks
+            ]
             if not remaining_tasks:
                 return
             chosen_task = random.choice(remaining_tasks)
@@ -156,7 +209,9 @@ class App:
                 return
             seen_tasks.add(chosen_task)
             self.list_all_tasks([chosen_task])
-            new_stress = self.get_numerical_prompt("Enter new stress level for task: ", also_accept=["x", ""])
+            new_stress = self.get_numerical_prompt(
+                "Enter new stress level for task: ", also_accept=["x", ""]
+            )
             if new_stress == "x":
                 return
             found_task = self.find_task(chosen_task.title)
@@ -164,19 +219,37 @@ class App:
                 found_task.stress = new_stress
 
     def reset_screen(self):
-        os.system('clear')
+        os.system("clear")
         print("\nWelcome to Procrastinator's Companion\n")
-        
+
+    def edit_task(self, task: Task):
+        task.title = rlinput(task.title, "Title:")
+
+        task.description = rlinput(task.description, "Description")
+        task.difficulty = self.get_numerical_prompt(
+            "",
+            input_func=lambda *args, **kwargs: rlinput(task.difficulty, "Difficulty:"),
+        )
+        task.stress = self.get_numerical_prompt(
+            "", input_func=lambda *args, **kwargs: rlinput(task.stress, "Stress:")
+        )
+        task.duration = self.get_numerical_prompt(
+            "", input_func=lambda *args, **kwargs: rlinput(task.duration, "Duration:")
+        )
+        task.complete = bool(rlinput(str(task.is_complete), "Is Complete:"))
+
     def display_home(self):
         print("\n")
-        command = input("Enter your command (new = n, list = ls, digit = task, xdigit = complete, ddigit = delete, s = save, r = refresh): ")
+        command = input(
+            "Enter your command (new = n, list = ls, digit = task, xdigit = complete, ddigit = delete, s = save, r = refresh): "
+        )
         self.reset_screen()
 
         if len(command) == 0:
             return
-        if command == 'n':
+        if command == "n":
             self.all_tasks.append(self.create_new_task())
-        if command == 'ls':
+        if command == "ls":
             self.list_all_tasks()
         if self._is_number(command):
             selected_task = self.cached_listed_tasks.get(int(command))
@@ -191,6 +264,12 @@ class App:
             selected_task = self.cached_listed_tasks.get(int(index_val))
             self.delete_task(selected_task.title)
             print("\nTask deleted.")
+        if command == "exit":
+            exit()
+        if command.startswith("e"):
+            index_val = command.split("e")[1]
+            selected_task = self.cached_listed_tasks.get(int(index_val))
+            self.edit_task(selected_task)
         if command == "s":
             self.save()
             print("Saved.")
@@ -199,14 +278,12 @@ class App:
             self.list_all_tasks()
         if command == "w":
             self.wizard()
-        if command == "exit":
-            exit()
         if command == "r":
             self.refresh_stress_levels()
         return
 
 
-os.system('clear')
+os.system("clear")
 app = App()
 app.load()
 app.list_all_tasks()
