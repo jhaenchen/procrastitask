fd0144a1881c95cdfc745dcab9c0ba3679cf446b
Jackson Haenchen <jackson.haenchen@ro.co>
2024-02-23 17:31:43 -0600

    Logging works

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass, field
 import math
 import os
@@ -9,6 +10,9 @@ import uuid
 import icalendar
 
 from dynamics.base_dynamic import BaseDynamic
+
+log = logging.getLogger()
+
 
 @dataclass
 class Task:
