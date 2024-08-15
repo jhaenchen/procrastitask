70356c51813baba25c396779aa1eafe5edd94157
Jackson Haenchen <jacksonhaenchen@gmail.com>
2024-08-14 23:19:25 -0500

    class style test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
 
+import unittest
 from procrastitask.procrastitask import App
 
-def test_can_initialize_app():
-    App()
+
+class TestApp(unittest.TestCase):
+    def test_can_initialize_app(self):
+        App()
