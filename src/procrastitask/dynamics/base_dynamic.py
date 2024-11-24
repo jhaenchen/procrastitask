dc06ee60d21d5f5ce71e09ae44ba2eac7112755a
Jackson Haenchen <jacksonhaenchen@gmail.com>
2024-11-23 21:35:24 -0600

    Allow clients to retrieve dynamic prefixes allowed

--- src/procrastitask/dynamics/base_dynamic.py
+++ src/procrastitask/dynamics/base_dynamic.py
@@ -16,20 +16,34 @@ class BaseDynamic(ABC):
     @abstractmethod
     def prefixes(self) -> List[str]:
         raise NotImplementedError()
+    
+    @staticmethod
+    def get_all_dynamics() -> List[type["BaseDynamic"]]:
+        from .linear_dynamic import LinearDynamic
+        from .linear_with_peak_dynamic import LinearWithPeakDynamic
+        all_dynamics = []
+        for class_obj in BaseDynamic.__subclasses__():
+            all_dynamics.append(class_obj)
+        return all_dynamics
+    
+    @staticmethod
+    def get_all_prefixes() -> List[str]:
+        all_prefixes = []
+        for class_obj in BaseDynamic.get_all_dynamics():
+            all_prefixes.extend(class_obj.prefixes)
+        return all_prefixes
 
     @staticmethod
     def find_dynamic(text: str) -> Optional["BaseDynamic"]:
-        from .linear_dynamic import LinearDynamic
-        from .linear_with_peak_dynamic import LinearWithPeakDynamic
-        all_prefixes = []
-        for class_obj in BaseDynamic.__subclasses__():
+        all_dynamics = BaseDynamic.get_all_dynamics()
+
+        for class_obj in all_dynamics:
             try:
-                all_prefixes.extend(class_obj.prefixes)
                 return class_obj.from_text(text)
             except:
                 pass
         if text:
-            raise ValueError(f"Provided dynamic text {text} could not be matched to a dynamic. Available: {all_prefixes}")
+            raise ValueError(f"Provided dynamic text {text} could not be matched to a dynamic. Available: {BaseDynamic.get_all_prefixes()}")
         return None
 
     @staticmethod
